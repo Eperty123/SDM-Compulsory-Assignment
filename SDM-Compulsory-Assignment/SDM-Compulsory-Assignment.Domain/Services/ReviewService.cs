2a89d5ee086cf88cbf4889974abd9da12827f376
Onii-chan <failer007@live.dk>
2021-10-04 12:00:55 +0200

    Implemented all service methods & tests

--- SDM-Compulsory-Assignment/SDM-Compulsory-Assignment.Domain/Services/ReviewService.cs
+++ SDM-Compulsory-Assignment/SDM-Compulsory-Assignment.Domain/Services/ReviewService.cs
@@ -25,7 +25,7 @@ namespace SDM_Compulsory_Assignment.Domain.Services
         public double GetAverageRateFromReviewer(int reviewer)
         {
             // Total
-            var reviewerReviews = _DataCRUD.ReadAll().ToList().FindAll(x => x.Reviewer == reviewer).ToList();
+            var reviewerReviews = _DataCRUD.ReadAll().Where(x => x.Reviewer == reviewer).ToList();
             int totalScore = 0;
             reviewerReviews.ForEach((review) =>
             {
@@ -37,7 +37,7 @@ namespace SDM_Compulsory_Assignment.Domain.Services
         public double GetAverageRateOfMovie(int movie)
         {
             // Total
-            var movies = _DataCRUD.ReadAll().ToList().FindAll(x => x.Movie == movie).ToList();
+            var movies = _DataCRUD.ReadAll().Where(x => x.Movie == movie).ToList();
             int totalScore = 0;
             movies.ForEach((review) =>
             {
@@ -77,53 +77,63 @@ namespace SDM_Compulsory_Assignment.Domain.Services
                 if (entry.Value == highestAmount)
                     ids.Add(entry.Key);
             }
-
-            //return dict.OrderByDescending(x => x.Key).ToList();
-            //return _DataCRUD.ReadAll().GroupBy(i => i.Reviewer).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).ToList();
             return ids;
         }
 
         public List<int> GetMoviesWithHighestNumberOfTopRates()
         {
             var ids = new List<int>();
-            var topMovies = _DataCRUD.ReadAll().ToList().FindAll(x => x.Grade == 5).OrderBy(x => x.Movie).ToList();
+            var topMovies = _DataCRUD.ReadAll().Where(x => x.Grade == 5).OrderBy(x => x.Movie).ToList();
             topMovies.ForEach(x => ids.Add(x.Movie));
             return ids;
         }
 
         public int GetNumberOfRates(int movie, int rate)
         {
-            return _DataCRUD.ReadAll().ToList().FindAll(x => x.Movie == movie && x.Grade == rate).Count;
+            return _DataCRUD.ReadAll().Where(x => x.Movie == movie && x.Grade == rate).Count();
         }
 
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            return _DataCRUD.ReadAll().ToList().FindAll(x => x.Reviewer == reviewer && x.Grade == rate).Count;
+            return _DataCRUD.ReadAll().Where(x => x.Reviewer == reviewer && x.Grade == rate).Count();
         }
 
         public int GetNumberOfReviews(int movie)
         {
-            return _DataCRUD.ReadAll().ToList().FindAll(x => x.Movie == movie).Count;
+            return _DataCRUD.ReadAll().Where(x => x.Movie == movie).Count();
         }
 
         public int GetNumberOfReviewsFromReviewer(int reviewer)
         {
-            return _DataCRUD.ReadAll().ToList().FindAll(x => x.Reviewer == reviewer).Count;
+            return _DataCRUD.ReadAll().Where(x => x.Reviewer == reviewer).Count();
         }
 
         public List<int> GetReviewersByMovie(int movie)
         {
-            throw new NotImplementedException();
+            return _DataCRUD.ReadAll().Where(x => x.Movie == movie).OrderByDescending(x => x.Grade).ThenByDescending(x => x.ReviewDate).Select(x => x.Reviewer).ToList();
         }
 
         public List<int> GetTopMoviesByReviewer(int reviewer)
         {
-            throw new NotImplementedException();
+            return _DataCRUD.ReadAll().Where(x => x.Reviewer == reviewer).OrderByDescending(x => x.Grade).ThenByDescending(x => x.ReviewDate).Select(x => x.Movie).ToList();
         }
 
         public List<int> GetTopRatedMovies(int amount)
         {
-            throw new NotImplementedException();
+            var averageRates = new Dictionary<int, double>();
+            var movies = _DataCRUD.ReadAll().OrderBy(x => x.Grade).Take(amount).ToList();
+            int currentAmount = 0;
+            for (int i = 0; i < movies.Count; i++)
+            {
+                var movie = movies[i];
+                var averageRating = GetAverageRateOfMovie(movie.Movie);
+                if (!averageRates.ContainsKey(movie.Movie))
+                {
+                    currentAmount++;
+                    averageRates.Add(movie.Movie, averageRating);
+                }
+            }
+            return averageRates.OrderByDescending(x => x.Value).Select(x => x.Key).ToList();
         }
     }
 }
--- SDM-Compulsory-Assignment/SDM-Compulsory-Assignment.Tests/UnitTest1.cs
+++ SDM-Compulsory-Assignment/SDM-Compulsory-Assignment.Tests/UnitTest1.cs
@@ -112,6 +112,28 @@ namespace SDM_Compulsory_Assignment.Tests
             Assert.Equal(expectedResult, actualResult);
         }
 
+
+        [Fact]
+        public void TestGetNumberOfRates()
+        {
+            Mock<IDataCRUD> reviewMock = new Mock<IDataCRUD>();
+            var reviewService = new ReviewService(reviewMock.Object);
+
+            var test = new List<Review>()
+            {
+                new Review() {Reviewer = 2, Grade = 4, Movie = 666, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 4, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 6, Movie = 4, ReviewDate = DateTime.Now},
+            };
+            reviewMock.Setup(m => m.ReadAll()).Returns(() => test);
+
+            //act
+            double actualResult = reviewService.GetNumberOfRates(666, 4);
+            reviewMock.Verify(x => x.ReadAll(), Times.Once);
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
         [Fact]
         public void TestGetAverageRateOfMovie()
         {
@@ -176,7 +198,84 @@ namespace SDM_Compulsory_Assignment.Tests
             //act
             var actualResult = reviewService.GetMostProductiveReviewers().Count;
             reviewMock.Verify(x => x.ReadAll(), Times.Once);
-            var expectedResult = 1;
+            var expectedResult = 5;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestGetTopRatedMovies()
+        {
+            Mock<IDataCRUD> reviewMock = new Mock<IDataCRUD>();
+            var reviewService = new ReviewService(reviewMock.Object);
+
+            var test = new List<Review>()
+            {
+                new Review() {Reviewer = 2, Grade = 5, Movie = 666, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 3, ReviewDate = DateTime.Now},
+                /*new Review() {Reviewer = 1, Grade = 4, Movie = 4, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 4, Grade = 3, Movie = 6, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 5, Grade = 1, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 5, Grade = 0, Movie = 7, ReviewDate = DateTime.Now},*/
+            };
+            reviewMock.Setup(m => m.ReadAll()).Returns(() => test);
+
+            //act
+            var actualResult = reviewService.GetTopRatedMovies(5).Count;
+            reviewMock.Verify(x => x.ReadAll(), Times.AtLeastOnce);
+            var expectedResult = 2;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestGetTopMoviesByReviewer()
+        {
+            Mock<IDataCRUD> reviewMock = new Mock<IDataCRUD>();
+            var reviewService = new ReviewService(reviewMock.Object);
+
+            var test = new List<Review>()
+            {
+                new Review() {Reviewer = 2, Grade = 5, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 1, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 5, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 5, Movie = 6, ReviewDate = DateTime.Now},
+                /*new Review() {Reviewer = 1, Grade = 4, Movie = 4, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 4, Grade = 3, Movie = 6, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 5, Grade = 1, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 5, Grade = 0, Movie = 7, ReviewDate = DateTime.Now},*/
+            };
+            reviewMock.Setup(m => m.ReadAll()).Returns(() => test);
+
+            //act
+            var actualResult = reviewService.GetTopMoviesByReviewer(2).Count;
+            reviewMock.Verify(x => x.ReadAll(), Times.AtLeastOnce);
+            var expectedResult = 5;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestGetReviewersByMovie()
+        {
+            Mock<IDataCRUD> reviewMock = new Mock<IDataCRUD>();
+            var reviewService = new ReviewService(reviewMock.Object);
+
+            var test = new List<Review>()
+            {
+                new Review() {Reviewer = 2, Grade = 5, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 2, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 5, Movie = 2, ReviewDate = DateTime.Now},
+            };
+            reviewMock.Setup(m => m.ReadAll()).Returns(() => test);
+
+            //act
+            var actualResult = reviewService.GetReviewersByMovie(2).Count;
+            reviewMock.Verify(x => x.ReadAll(), Times.AtLeastOnce);
+            var expectedResult = 3;
             Assert.Equal(expectedResult, actualResult);
         }
     }
